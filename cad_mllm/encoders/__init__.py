f84b96b66c1c3869605faad5e9c8ee60f1ba43d5
karthick_raja <233344860+kbangaru-cyber@users.noreply.github.com>
2025-11-18 15:42:56 -0500

    Update __init__.py

--- cad_mllm/encoders/__init__.py
+++ cad_mllm/encoders/__init__.py
@@ -1,3 +1,3 @@
 from .image_encoder import ImageEncoder
 from .text_encoder import TextEncoder
-from .pointcloud_encoder import PointCloudEncoder
+from .pointcloud_encoder import MichelangeloPointEncoder
